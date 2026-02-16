f6b3cebf09a2402c9d29c7fd368738164fdfc75b
Wong Wei Jun <wong.weijun923@gmail.com>
2026-02-16 08:55:32 +0800

    Added database functionalities

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Depends
+from typing import Sequence
+from fastapi import FastAPI, Depends, Query, HTTPException
 import psycopg2
 import os
-from sqlmodel import SQLModel,Session, select
-from database import engine, get_db
-import models
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from .database import engine, get_db
+from .models import Pipelines, Jobs
+from uuid import UUID
 
 SQLModel.metadata.create_all(engine)
 
@@ -13,24 +15,32 @@ app = FastAPI()
 def checkConnection(db: Session = Depends(get_db)):
     try:
         db.exec(select(1)) 
-        return {"status": "Connected to Database via SQLModel!"}
+        return {"status": "Connected to Database!"}
     except Exception as e:
         return {"error": str(e)}
-@app.post("/pipelines")
-async def showPipelines(db: Session= Depends(get_db)):
-    pass
 
+@app.post("/pipelines")
+async def createPipelines(pipeline: Pipelines, db: Session= Depends(get_db)) -> Pipelines:
+    db.add(pipeline)
+    db.commit()
+    db.refresh(pipeline)
+    return pipeline
 
 @app.get("/pipelines")
-async def getPipelines(db: Session = Depends(get_db)):
-    pass
-
+async def getPipelines(db: Session = Depends(get_db), offset: int=0, limit: int = Query(default=100, le=100),) -> Sequence[Pipelines]:
+    pipelines = db.exec(select(Pipelines).offset(offset).limit(limit)).all()
+    return pipelines
 
 @app.get("/pipelines/{id}")
-async def getPipeline(user_id: int, db: Session= Depends(get_db)):
-    pass
-
+async def getPipeline(pipe_id: UUID, db: Session= Depends(get_db)):
+    pipelines = db.exec(select(Pipelines).where(Pipelines.id == pipe_id)).all()
+    return pipelines
 
 @app.delete("/pipelines/{id}")
-async def deletePipeline(user_id: int, db: Session= Depends(get_db)):
-    pass
+async def deletePipeline(pipe_id: UUID, db: Session= Depends(get_db)):
+    pipeline = db.get(Pipelines, pipe_id)
+    if not pipeline:
+        raise HTTPException(status_code=404, detail="Pipeline not Found")
+    db.delete(pipeline)
+    db.commit()
+    return {"ok": True}
