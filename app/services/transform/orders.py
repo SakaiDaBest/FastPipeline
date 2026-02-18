a3d8f28e395be10bb900f197b1600b650616fb01
Wong Wei Jun <wong.weijun923@gmail.com>
2026-02-18 09:27:43 +0800

    Added return

--- app/services/transform/orders.py
+++ app/services/transform/orders.py
@@ -8,3 +8,5 @@ def cleanOrders(df):
     if 'total_amount' in df.columns:
         df['total_amount'] = df['total_amount'].str.extract(r'(\d+\.?\d*)').astype(float)
     df.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
+
+    return df
