b6c3d39bfd4232e45b9f46b7c987b882529d18eb
Wong Wei Jun <wong.weijun923@gmail.com>
2026-03-04 14:57:11 +0800

    initial commit

---
+++ app/services/extract/openMateo.py
@@ -0,0 +1,21 @@
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 2.5,
+	"longitude": 112.5,
+	"hourly": "temperature_2m",
+	"timezone": "Asia/Singapore",
+}
+responses = openmeteo.weather_api(url, params=params)
